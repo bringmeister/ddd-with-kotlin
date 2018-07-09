ab3ece62d9147c77324b925e85bd6141bfa27ee2
Joerg Bellmann <joerg.bellmann@zalando.de>
2018-07-09 15:44:59 +0200

    Update Spring-Boot version to 2.0.3.RELEASE, kotlin deps to 1.2.51, exclude log4j2

--- src/main/kotlin/de/bringmeister/connect/product/Application.kt
+++ src/main/kotlin/de/bringmeister/connect/product/Application.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
-@EnableAutoConfiguration
+// @EnableAutoConfiguration
 @EnableAsync
 class Application
 
